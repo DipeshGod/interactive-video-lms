7eeda73a40bd8340ecfe4eff593de1e572ac4901
CodeAddictDipesh <45348267+DipesAtcodeict@users.noreply.github.com>
2021-06-15 13:00:50 +0545

    little bit

--- frontend/components/courses/CourseContent.tsx
+++ frontend/components/courses/CourseContent.tsx
@@ -1,6 +1,15 @@
 import { useRouter } from 'next/router';
 import { useQuery } from 'react-query';
+import {
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Typography,
+} from '@material-ui/core';
 import getCourseContent from '../../services/client/course/getCourseContent';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import VideoPlayer from '../videoPlayer';
+import Loading from '../Loading';
 
 const CourseContent = () => {
   const router = useRouter();
@@ -10,11 +19,32 @@ const CourseContent = () => {
     getCourseContent(courseId)
   );
 
+  if (isLoading) {
+    return <Loading />;
+  }
+
   console.log(data);
 
   return (
-    <div>
-      <h2>This is course content</h2>
+    <div style={{ marginBottom: '3rem' }}>
+      {data.map((module) => (
+        <Accordion key={module._id}>
+          <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+            <Typography variant='h6'>{module.title}</Typography>
+          </AccordionSummary>
+          <AccordionDetails>{module.description}</AccordionDetails>
+
+          {module.videos.map((video) => (
+            <AccordionDetails key={video.title}>
+              <VideoPlayer
+                poster={``}
+                videoSources={video}
+                videoTitle={''}
+              />
+            </AccordionDetails>
+          ))}
+        </Accordion>
+      ))}
     </div>
   );
 };
