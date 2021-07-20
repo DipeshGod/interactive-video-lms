7e0892c77e9db36f51213f77bebc2b44e902b92c
CodeAddictDipesh <45348267+DipesAtcodeict@users.noreply.github.com>
2021-07-20 13:06:02 +0545

    hhhhh

--- backend/src/interfaces/models/Progress.ts
+++ backend/src/interfaces/models/Progress.ts
@@ -1,47 +1,50 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 export interface IStudentProgress {
-    userId: string;
-    courseId: string;
-    preTest: {
-        score: number;
-        solvedQuestions: number;
-        totalQuestions: number;
+  userId: string;
+  courseId: string;
+  preTest: {
+    score: number;
+    solvedQuestions: number;
+    totalQuestions: number;
+  };
+  finalTestScore: {
+    score: number;
+    solvedQuestions: number;
+    totalQuestions: number;
+  };
+  module: [
+    {
+      id: string;
+      score: number;
+      solvedQuestions: number;
+      totalQuestions: number;
     }
-    finalTestScore: {
-        score: number;
-        solvedQuestions: number;
-        totalQuestions: number;
-    }
-    module: [{
-        id: string;
-        score: number;
-        solvedQuestions: number;
-        totalQuestions: number;
-
-    }];
+  ];
 }
 
 export interface IStudentProgressDoc extends mongoose.Document {
-    userId: string;
-    courseId: string;
-    preTest: {
-        score: number;
-        solvedQuestions: number;
-        totalQuestions: number;
+  userId: string;
+  courseId: string;
+  preTest: {
+    score: number;
+    solvedQuestions: number;
+    totalQuestions: number;
+  };
+  finalTestScore: {
+    score: number;
+    solvedQuestions: number;
+    totalQuestions: number;
+  };
+  module: [
+    {
+      id: string;
+      score: number;
+      solvedQuestions: number;
+      totalQuestions: number;
     }
-    finalTestScore: {
-        score: number;
-        solvedQuestions: number;
-        totalQuestions: number;
-    }
-    module: [{
-        id: string;
-        score: number;
-        solvedQuestions: number;
-        totalQuestions: number;
-
-    }];
+  ];
 }
 
-export interface IStudentProgressModel extends mongoose.Model<IStudentProgressDoc> { }
+export interface IStudentProgressModel
+  extends mongoose.Model<IStudentProgressDoc> {}
--- backend/src/models/Progress.ts
+++ backend/src/models/Progress.ts
@@ -1,77 +1,90 @@
-import mongoose, { Schema } from "mongoose";
-import { IStudentProgressDoc, IStudentProgressModel } from "../interfaces/models/Progress";
+import mongoose, { Schema } from 'mongoose';
+import {
+  IStudentProgressDoc,
+  IStudentProgressModel,
+} from '../interfaces/models/Progress';
 
-const progressSchema = new mongoose.Schema({
+const moduleProgressSchema = new mongoose.Schema({
+  id: {
+    type: Schema.Types.ObjectId,
+    ref: 'module',
+  },
+  score: {
+    type: Number,
+    min: 0,
+    max: 100,
+    default: 0,
+  },
+  solvedQuestions: {
+    type: Number,
+    min: 0,
+    default: 0,
+  },
+  totalQuestions: {
+    type: Number,
+    min: 0,
+    default: 0,
+  },
+});
+
+const progressSchema = new mongoose.Schema(
+  {
     userId: {
-        type: Schema.Types.ObjectId,
-        ref: 'user',
-        required: true
+      type: Schema.Types.ObjectId,
+      ref: 'user',
+      required: true,
     },
     courseId: {
-        type: Schema.Types.ObjectId,
-        ref: 'course',
-        required: true
+      type: Schema.Types.ObjectId,
+      ref: 'course',
+      required: true,
     },
     preTestScore: {
-        score: {
-            type: Number,
-            min: 0,
-            max: 100,
-            default: 0
-        },
-        solvedQuestions: {
-            type: Number,
-            min: 0,
-            default: 0
-        },
-        totalQuestions: {
-            type: Number,
-            min: 0,
-            default: 0
-        }
+      score: {
+        type: Number,
+        min: 0,
+        max: 100,
+        default: 0,
+      },
+      solvedQuestions: {
+        type: Number,
+        min: 0,
+        default: 0,
+      },
+      totalQuestions: {
+        type: Number,
+        min: 0,
+        default: 0,
+      },
     },
     finalTestScore: {
-        score: {
-            type: Number,
-            min: 0,
-            max: 100,
-            default: 0
-        },
-        solvedQuestions: {
-            type: Number,
-            min: 0,
-            default: 0
-        },
-        totalQuestions: {
-            type: Number,
-            min: 0,
-            default: 0
-        }
+      score: {
+        type: Number,
+        min: 0,
+        max: 100,
+        default: 0,
+      },
+      solvedQuestions: {
+        type: Number,
+        min: 0,
+        default: 0,
+      },
+      totalQuestions: {
+        type: Number,
+        min: 0,
+        default: 0,
+      },
     },
-    module: [{
-        id: {
-            type: Schema.Types.ObjectId,
-            ref: 'module'
-        },
-        score: {
-            type: Number,
-            min: 0,
-            max: 100,
-            default: 0
-        },
-        solvedQuestions: {
-            type: Number,
-            min: 0,
-            default: 0
-        },
-        totalQuestions: {
-            type: Number,
-            min: 0,
-            default: 0
-        }
-    }],
-}, { timestamps: true })
+    module: {
+      type: [moduleProgressSchema],
+    },
+  },
+  { timestamps: true }
+);
 
-const StudentProgress = mongoose.model<IStudentProgressDoc, IStudentProgressModel>('progress', progressSchema);
+const StudentProgress = mongoose.model<
+  IStudentProgressDoc,
+  IStudentProgressModel
+>('progress', progressSchema);
 
-export { StudentProgress }
+export { StudentProgress };
