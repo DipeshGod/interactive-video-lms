13df69697e2899cc03d4c3e7cbb382061f39bd8e
dipesh chaulagain <codeict.dipesh@gmail.com>
2021-12-16 15:50:21 +0545

    check

--- frontend/pages/course/[slug].tsx
+++ frontend/pages/course/[slug].tsx
@@ -1,42 +1,40 @@
 import { Button, Container, Box } from '@material-ui/core';
-import { QueryClient } from 'react-query';
-import { dehydrate } from 'react-query/hydration';
+import { useQuery } from 'react-query';
 import Layout from '../../components/layout';
 import CourseIntro from '../../components/courses/CourseIntro';
-import CourseReview from '../../components/courses/CourseReview';
 import getCoursesById from '../../services/client/course/getCourseById';
+import CourseReview from '../../components/courses/CourseReview';
+import Loading from '../../components/Loading';
+import { useRouter } from 'next/router';
 
-const CourseDetails = ({ course, id }) => {
+const CourseDetails = () => {
+  const router = useRouter();
+
+  const { isLoading, data } = useQuery(['courses', router.query.slug], () =>
+    getCoursesById(router.query.slug)
+  );
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  console.log('data', data);
+
   return (
     <Layout>
       <div style={{ padding: '3rem 0', minHeight: '80vh' }}>
         <Container>
-          <CourseIntro course={course} />
+          <CourseIntro course={data} />
           <Box marginTop='2rem'>
             <Button variant='contained' color='primary' size='large'>
-              ENROLL NOW for Rs. {course.price}
+              ENROLL NOW for Rs. {data.price}
             </Button>
           </Box>
-          <CourseReview id={id} />
+          <CourseReview id={data._id} />
         </Container>
       </div>
     </Layout>
   );
 };
-
-export async function getServerSideProps({ params }) {
-  const queryClient = new QueryClient();
-
-  await queryClient.prefetchQuery(['course', params.slug], () =>
-    getCoursesById(params.slug)
-  );
-
-  return {
-    props: {
-      course: dehydrate(queryClient).queries[0].state.data,
-      id: params.slug,
-    },
-  };
-}
 
 export default CourseDetails;
